7a35a484074365a7e0e3b408aa17c799c89d6243
Aaron Chafetz <achafetz@usaid.gov>
2022-08-15 15:41:04 -0400

    feat: add fcn to dynamically set font if Source Sans Pro is not available

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,8 @@
 {
   check_fonts()
 
+  choose_font()
+
   invisible ()
 }
 
@@ -24,6 +26,41 @@ check_fonts <- function(){
 
     #check if font (Source Sans Pro) is installed
     if(!"Source Sans Pro" %in% localfonts)
-      usethis::ui_warn("Before proceeding, install font: Source Sans Pro")
+      usethis::ui_warn("Before proceeding, we recommend installing the Source Sans Pro from USAID Software Center or Google Fonts. In the absense of Source Sans Pro, we will default to using Arial")
 
+}
+
+
+#' Check for Imported Fonts
+#'
+#' @keywords internal
+#'
+choose_font <- function(){
+
+  localfonts <- extrafont::fonts()
+
+  ssp_exists <- "Source Sans Pro" %in% localfonts
+
+  if(.Platform$OS.type == "windows" && ssp_exists){
+    windowsFonts(glitr_font_default = windowsFont("Source Sans Pro"))
+    return(invisible())
+  }
+
+  if(.Platform$OS.type == "windows" && !ssp_exists){
+    windowsFonts(glitr_font_default = windowsFonts()$sans)
+    return(invisible())
+  }
+
+  if(.Platform$OS.type == "unix" && ssp_exists){
+    grDevices::quartzFonts(glitr_font_default = c("Source Sans Pro"))
+    return(invisible())
+  }
+
+  if(.Platform$OS.type == "unix" && !ssp_exists){
+    grDevices::quartzFonts(glitr_font_default = grDevices::quartzFonts("sans"))
+    return(invisible())
+  }
+  # default_font <- ifelse(ssp_exists, "Source Sans Pro", "Arial")
+  #
+  # options("glitr_font" = default_font)
 }
