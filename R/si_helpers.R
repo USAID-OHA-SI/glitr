fada3eb7b69db5e59b1665aabe3ef5994379e079
Aaron Chafetz <achafetz@usaid.gov>
2024-07-16 11:31:18 -0400

    feat: add default na value to scale_*_si

--- R/si_helpers.R
+++ R/si_helpers.R
@@ -94,11 +94,11 @@ si_rampr <- function(pal_name = "siei", n, alpha = 1, reverse = FALSE) {
 #' @param reverse if true, reverses the order of palette
 #' @param discrete whether to use a discrete color palette
 #' @param alpha sets transparency of each color
+#' @param na.value color for NA values, default = slate
 #' @param ... additional arguments to pass to scale_color_gradientn
 #'
-#' @inheritParams viridis::scale_color_viridis
-#'
-#' @importFrom ggplot2 scale_color_manual discrete_scale scale_color_gradientn
+#' @importFrom ggplot2 discrete_scale
+#' @importFrom ggplot2 scale_color_gradientn
 #' @export
 #'
 #' @examples
@@ -116,19 +116,17 @@ si_rampr <- function(pal_name = "siei", n, alpha = 1, reverse = FALSE) {
 #' scale_color_si("hunter_c")}
 #'
 
-  scale_color_si <- function(palette = "hunter_c", alpha = 1, discrete = FALSE, reverse = FALSE, ...) {
+  scale_color_si <- function(palette = "hunter_c", alpha = 1, discrete = FALSE, reverse = FALSE, na.value = slate, ...) {
+
+    if(missing(na.value))
 
     if (discrete == TRUE) {
-      discrete_scale("colour", "si_rampr", si_pal(palette, alpha = alpha, reverse = reverse), ...)
+      discrete_scale("colour", "si_rampr", si_pal(palette, alpha = alpha, reverse = reverse), na.value = na.value, ...)
     }
     else {
-      scale_color_gradientn(colours = si_rampr(palette, 256, alpha = alpha, reverse = reverse), ...)
+      scale_color_gradientn(colours = si_rampr(palette, 256, alpha = alpha, reverse = reverse), na.value = na.value, ...)
     }
   }
-
-
-#' @export
-  scale_color_si <- scale_color_si
 
 
 #' @title  scale_fill_si
@@ -140,12 +138,12 @@ si_rampr <- function(pal_name = "siei", n, alpha = 1, reverse = FALSE) {
 #' @param alpha sets transparency of each color
 #' @param reverse if TRUE, reverses order of palette
 #' @param discrete whether to use a discrete colour palette
+#' @param na.value color for NA values, default = slate
 #' @param ... additional arguments to pass to scale_color_gradientn
 #'
-#' @inheritParams viridis::scale_fill_viridis
-#' @inheritParams si_pal
+#' @importFrom ggplot2 discrete_scale
+#' @importFrom ggplot2 scale_color_gradientn
 #'
-#' @importFrom ggplot2 scale_fill_manual discrete_scale scale_fill_gradientn
 #' @export
 #'
 #' @examples
@@ -169,18 +167,16 @@ si_rampr <- function(pal_name = "siei", n, alpha = 1, reverse = FALSE) {
 #'  si_style_void()
 #'  }
 
-  scale_fill_si <- function(palette = "hunter_c", alpha = 1, discrete = FALSE, reverse = FALSE, ...) {
+  scale_fill_si <- function(palette = "hunter_c", alpha = 1, discrete = FALSE, reverse = FALSE, na.value = slate, ...) {
 
     if (discrete == TRUE) {
-      discrete_scale("fill", "si_rampr", si_pal(palette, alpha = alpha, reverse = reverse), ...)
+      ggplot2::discrete_scale("fill", "si_rampr", si_pal(palette, alpha = alpha, reverse = reverse), na.value = na.value, ...)
     }
     else {
-      scale_fill_gradientn(colours = si_rampr(palette, 256, alpha = alpha, reverse = reverse), ...)
+      ggplot2::scale_fill_gradientn(colours = si_rampr(palette, 256, alpha = alpha, reverse = reverse), na.value = na.value, ...)
     }
   }
 
 
-#' @export
-  scale_fill_si <- scale_fill_si
 
 
